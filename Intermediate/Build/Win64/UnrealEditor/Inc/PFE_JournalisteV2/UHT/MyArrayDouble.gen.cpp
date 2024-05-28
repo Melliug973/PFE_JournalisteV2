756a9a881039d59095bd8eb76ab9ca9dd6753bad
Synnah31 <jules.ducuing@gmail.com>
2024-05-29 01:32:30 +0200

    Début niveau dialogue (+ regenerer visual)

--- Intermediate/Build/Win64/UnrealEditor/Inc/PFE_JournalisteV2/UHT/MyArrayDouble.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/PFE_JournalisteV2/UHT/MyArrayDouble.gen.cpp
@@ -30,6 +30,29 @@ void EmptyLinkFunctionForGeneratedCodeMyArrayDouble() {}
 		*(TArray<FText>*)Z_Param__Result=P_THIS->GetDoubleArrayText(Z_Param_Index);
 		P_NATIVE_END;
 	}
+	DEFINE_FUNCTION(UMyArrayDouble::execAddDoubleArrayInt)
+	{
+		P_GET_TARRAY(int32,Z_Param_ListInt);
+		P_FINISH;
+		P_NATIVE_BEGIN;
+		P_THIS->AddDoubleArrayInt(Z_Param_ListInt);
+		P_NATIVE_END;
+	}
+	DEFINE_FUNCTION(UMyArrayDouble::execGetDoubleArrayInt)
+	{
+		P_FINISH;
+		P_NATIVE_BEGIN;
+		*(int32*)Z_Param__Result=P_THIS->GetDoubleArrayInt();
+		P_NATIVE_END;
+	}
+	DEFINE_FUNCTION(UMyArrayDouble::execGetDoubleArrayIntIndex)
+	{
+		P_GET_PROPERTY(FIntProperty,Z_Param_Index);
+		P_FINISH;
+		P_NATIVE_BEGIN;
+		*(TArray<int32>*)Z_Param__Result=P_THIS->GetDoubleArrayIntIndex(Z_Param_Index);
+		P_NATIVE_END;
+	}
 	DEFINE_FUNCTION(UMyArrayDouble::execGetDoubleArrayString)
 	{
 		P_GET_PROPERTY(FIntProperty,Z_Param_Index);
@@ -50,12 +73,52 @@ void EmptyLinkFunctionForGeneratedCodeMyArrayDouble() {}
 	{
 		UClass* Class = UMyArrayDouble::StaticClass();
 		static const FNameNativePtrPair Funcs[] = {
+			{ "AddDoubleArrayInt", &UMyArrayDouble::execAddDoubleArrayInt },
 			{ "AddDoubleArrayString", &UMyArrayDouble::execAddDoubleArrayString },
 			{ "AddDoubleArrayText", &UMyArrayDouble::execAddDoubleArrayText },
+			{ "GetDoubleArrayInt", &UMyArrayDouble::execGetDoubleArrayInt },
+			{ "GetDoubleArrayIntIndex", &UMyArrayDouble::execGetDoubleArrayIntIndex },
 			{ "GetDoubleArrayString", &UMyArrayDouble::execGetDoubleArrayString },
 			{ "GetDoubleArrayText", &UMyArrayDouble::execGetDoubleArrayText },
 		};
 		FNativeFunctionRegistrar::RegisterFunctions(Class, Funcs, UE_ARRAY_COUNT(Funcs));
+	}
+	struct Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics
+	{
+		struct MyArrayDouble_eventAddDoubleArrayInt_Parms
+		{
+			TArray<int32> ListInt;
+		};
+		static const UECodeGen_Private::FIntPropertyParams NewProp_ListInt_Inner;
+		static const UECodeGen_Private::FArrayPropertyParams NewProp_ListInt;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Function_MetaDataParams[];
+#endif
+		static const UECodeGen_Private::FFunctionParams FuncParams;
+	};
+	const UECodeGen_Private::FIntPropertyParams Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::NewProp_ListInt_Inner = { "ListInt", nullptr, (EPropertyFlags)0x0000000000000000, UECodeGen_Private::EPropertyGenFlags::Int, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, 0, METADATA_PARAMS(0, nullptr) };
+	const UECodeGen_Private::FArrayPropertyParams Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::NewProp_ListInt = { "ListInt", nullptr, (EPropertyFlags)0x0010000000000080, UECodeGen_Private::EPropertyGenFlags::Array, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(MyArrayDouble_eventAddDoubleArrayInt_Parms, ListInt), EArrayPropertyFlags::None, METADATA_PARAMS(0, nullptr) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::NewProp_ListInt_Inner,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::NewProp_ListInt,
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::Function_MetaDataParams[] = {
+		{ "ModuleRelativePath", "MyArrayDouble.h" },
+	};
+#endif
+	const UECodeGen_Private::FFunctionParams Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::FuncParams = { (UObject*(*)())Z_Construct_UClass_UMyArrayDouble, nullptr, "AddDoubleArrayInt", nullptr, nullptr, Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::PropPointers, UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::PropPointers), sizeof(Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::MyArrayDouble_eventAddDoubleArrayInt_Parms), RF_Public|RF_Transient|RF_MarkAsNative, (EFunctionFlags)0x04020401, 0, 0, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::Function_MetaDataParams), Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::Function_MetaDataParams) };
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::PropPointers) < 2048);
+	static_assert(sizeof(Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::MyArrayDouble_eventAddDoubleArrayInt_Parms) < MAX_uint16);
+	UFunction* Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt()
+	{
+		static UFunction* ReturnFunction = nullptr;
+		if (!ReturnFunction)
+		{
+			UECodeGen_Private::ConstructUFunction(&ReturnFunction, Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt_Statics::FuncParams);
+		}
+		return ReturnFunction;
 	}
 	struct Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayString_Statics
 	{
@@ -128,6 +191,81 @@ void EmptyLinkFunctionForGeneratedCodeMyArrayDouble() {}
 		if (!ReturnFunction)
 		{
 			UECodeGen_Private::ConstructUFunction(&ReturnFunction, Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayText_Statics::FuncParams);
+		}
+		return ReturnFunction;
+	}
+	struct Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics
+	{
+		struct MyArrayDouble_eventGetDoubleArrayInt_Parms
+		{
+			int32 ReturnValue;
+		};
+		static const UECodeGen_Private::FIntPropertyParams NewProp_ReturnValue;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Function_MetaDataParams[];
+#endif
+		static const UECodeGen_Private::FFunctionParams FuncParams;
+	};
+	const UECodeGen_Private::FIntPropertyParams Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::NewProp_ReturnValue = { "ReturnValue", nullptr, (EPropertyFlags)0x0010000000000580, UECodeGen_Private::EPropertyGenFlags::Int, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(MyArrayDouble_eventGetDoubleArrayInt_Parms, ReturnValue), METADATA_PARAMS(0, nullptr) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::NewProp_ReturnValue,
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::Function_MetaDataParams[] = {
+		{ "ModuleRelativePath", "MyArrayDouble.h" },
+	};
+#endif
+	const UECodeGen_Private::FFunctionParams Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::FuncParams = { (UObject*(*)())Z_Construct_UClass_UMyArrayDouble, nullptr, "GetDoubleArrayInt", nullptr, nullptr, Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::PropPointers, UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::PropPointers), sizeof(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::MyArrayDouble_eventGetDoubleArrayInt_Parms), RF_Public|RF_Transient|RF_MarkAsNative, (EFunctionFlags)0x04020401, 0, 0, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::Function_MetaDataParams), Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::Function_MetaDataParams) };
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::PropPointers) < 2048);
+	static_assert(sizeof(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::MyArrayDouble_eventGetDoubleArrayInt_Parms) < MAX_uint16);
+	UFunction* Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt()
+	{
+		static UFunction* ReturnFunction = nullptr;
+		if (!ReturnFunction)
+		{
+			UECodeGen_Private::ConstructUFunction(&ReturnFunction, Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt_Statics::FuncParams);
+		}
+		return ReturnFunction;
+	}
+	struct Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics
+	{
+		struct MyArrayDouble_eventGetDoubleArrayIntIndex_Parms
+		{
+			int32 Index;
+			TArray<int32> ReturnValue;
+		};
+		static const UECodeGen_Private::FIntPropertyParams NewProp_Index;
+		static const UECodeGen_Private::FIntPropertyParams NewProp_ReturnValue_Inner;
+		static const UECodeGen_Private::FArrayPropertyParams NewProp_ReturnValue;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Function_MetaDataParams[];
+#endif
+		static const UECodeGen_Private::FFunctionParams FuncParams;
+	};
+	const UECodeGen_Private::FIntPropertyParams Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::NewProp_Index = { "Index", nullptr, (EPropertyFlags)0x0010000000000080, UECodeGen_Private::EPropertyGenFlags::Int, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(MyArrayDouble_eventGetDoubleArrayIntIndex_Parms, Index), METADATA_PARAMS(0, nullptr) };
+	const UECodeGen_Private::FIntPropertyParams Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::NewProp_ReturnValue_Inner = { "ReturnValue", nullptr, (EPropertyFlags)0x0000000000000000, UECodeGen_Private::EPropertyGenFlags::Int, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, 0, METADATA_PARAMS(0, nullptr) };
+	const UECodeGen_Private::FArrayPropertyParams Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::NewProp_ReturnValue = { "ReturnValue", nullptr, (EPropertyFlags)0x0010000000000580, UECodeGen_Private::EPropertyGenFlags::Array, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(MyArrayDouble_eventGetDoubleArrayIntIndex_Parms, ReturnValue), EArrayPropertyFlags::None, METADATA_PARAMS(0, nullptr) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::NewProp_Index,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::NewProp_ReturnValue_Inner,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::NewProp_ReturnValue,
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::Function_MetaDataParams[] = {
+		{ "ModuleRelativePath", "MyArrayDouble.h" },
+	};
+#endif
+	const UECodeGen_Private::FFunctionParams Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::FuncParams = { (UObject*(*)())Z_Construct_UClass_UMyArrayDouble, nullptr, "GetDoubleArrayIntIndex", nullptr, nullptr, Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::PropPointers, UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::PropPointers), sizeof(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::MyArrayDouble_eventGetDoubleArrayIntIndex_Parms), RF_Public|RF_Transient|RF_MarkAsNative, (EFunctionFlags)0x04020401, 0, 0, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::Function_MetaDataParams), Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::Function_MetaDataParams) };
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::PropPointers) < 2048);
+	static_assert(sizeof(Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::MyArrayDouble_eventGetDoubleArrayIntIndex_Parms) < MAX_uint16);
+	UFunction* Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex()
+	{
+		static UFunction* ReturnFunction = nullptr;
+		if (!ReturnFunction)
+		{
+			UECodeGen_Private::ConstructUFunction(&ReturnFunction, Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex_Statics::FuncParams);
 		}
 		return ReturnFunction;
 	}
@@ -234,8 +372,11 @@ void EmptyLinkFunctionForGeneratedCodeMyArrayDouble() {}
 	};
 	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_UMyArrayDouble_Statics::DependentSingletons) < 16);
 	const FClassFunctionLinkInfo Z_Construct_UClass_UMyArrayDouble_Statics::FuncInfo[] = {
+		{ &Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayInt, "AddDoubleArrayInt" }, // 71128095
 		{ &Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayString, "AddDoubleArrayString" }, // 2731081210
 		{ &Z_Construct_UFunction_UMyArrayDouble_AddDoubleArrayText, "AddDoubleArrayText" }, // 2316626197
+		{ &Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayInt, "GetDoubleArrayInt" }, // 404677741
+		{ &Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayIntIndex, "GetDoubleArrayIntIndex" }, // 1867790878
 		{ &Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayString, "GetDoubleArrayString" }, // 624765010
 		{ &Z_Construct_UFunction_UMyArrayDouble_GetDoubleArrayText, "GetDoubleArrayText" }, // 1509562519
 	};
@@ -280,15 +421,15 @@ void EmptyLinkFunctionForGeneratedCodeMyArrayDouble() {}
 	}
 	DEFINE_VTABLE_PTR_HELPER_CTOR(UMyArrayDouble);
 	UMyArrayDouble::~UMyArrayDouble() {}
-	struct Z_CompiledInDeferFile_FID_PFE_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics
+	struct Z_CompiledInDeferFile_FID_Users_jules_Unreal_Projects_PFE_Journalisme_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics
 	{
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
-	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_PFE_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics::ClassInfo[] = {
-		{ Z_Construct_UClass_UMyArrayDouble, UMyArrayDouble::StaticClass, TEXT("UMyArrayDouble"), &Z_Registration_Info_UClass_UMyArrayDouble, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UMyArrayDouble), 3184317838U) },
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_jules_Unreal_Projects_PFE_Journalisme_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_UMyArrayDouble, UMyArrayDouble::StaticClass, TEXT("UMyArrayDouble"), &Z_Registration_Info_UClass_UMyArrayDouble, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UMyArrayDouble), 2685107120U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_PFE_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_2685566273(TEXT("/Script/PFE_JournalisteV2"),
-		Z_CompiledInDeferFile_FID_PFE_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_PFE_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics::ClassInfo),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_jules_Unreal_Projects_PFE_Journalisme_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_3338488110(TEXT("/Script/PFE_JournalisteV2"),
+		Z_CompiledInDeferFile_FID_Users_jules_Unreal_Projects_PFE_Journalisme_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_jules_Unreal_Projects_PFE_Journalisme_PFE_JournalisteV2_Source_PFE_JournalisteV2_MyArrayDouble_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Intermediate/Build/Win64/x64/PFE_JournalisteV2Editor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++ Intermediate/Build/Win64/x64/PFE_JournalisteV2Editor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/PFE/PFE_JournalisteV2/Intermediate/Build/Win64/x64/PFE_JournalisteV2Editor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "C:/Users/jules/Unreal Projects/PFE_Journalisme/PFE_JournalisteV2/Intermediate/Build/Win64/x64/PFE_JournalisteV2Editor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
